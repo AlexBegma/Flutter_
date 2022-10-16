dc3cba747a53ca656c41273ac6461803033e6320
Alex Behma <alexbegma@gmail.com>
2022-10-16 20:19:57 +0300

    Hello Word

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(new MaterialApp(
+    title: 'Hello World App',
+    home: new myApp(),
+  ));
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Hello World App'),
+      ),
+      body: new Center(
+        child: new Text('Hello, world!'),
+      ),
+    );
+  }
+}
